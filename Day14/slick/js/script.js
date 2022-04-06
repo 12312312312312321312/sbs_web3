3f0cdc0b2d4d8275de69f000dadc0e5e8ac14e57
12312312312312321312 <pig2451@gmail.com>
2022-04-06 21:52:02 +0900

    0406

--- Day14/slick/js/script.js
+++ Day14/slick/js/script.js
@@ -2,9 +2,15 @@
 
 $(function() {
 
+    //슬라이드 박스
+    let slide = $('.slide-container')
+
+    // 슬라이드 전체 개수
+    let slideCount = $('.slide').length / 2
+    $('#slideCount').text(slideCount)
 
     // 슬릭 슬라이드 시작!
-    $('.slide-container').slick({
+   slide.slick({
         dots: true,
         arrows: true,
 
@@ -14,11 +20,13 @@ $(function() {
         infinite: true,                 // 무한 반복 
         speed:500,                      // 슬라이드가 전환되는 시간
 
+        variableWidth: true,            // 가변적인 가로 사이즈 여부
+
 
         // centerMode: true,            // 센터 모드
         // centerPadding: '60px',       // 센터 모드 시, 내부여백
-        slidesToShow: 3,                // 보여질 슬라이드 개수
-        slidesToScroll: 1,              // 스크롤될 슬라이드 개수
+        slidesToShow: 2,                // 보여질 슬라이드 개수
+        slidesToScroll: 2,              // 스크롤될 슬라이드 개수
 
 
         pauseOnDotsHover: true,         // 페이지네이션 호버 시 자동재생 멈춤
@@ -41,7 +49,7 @@ $(function() {
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 3,
+                    slidesToShow: 2,
                     slidesToScroll: 1,
                 }
             },
@@ -73,6 +81,23 @@ $(function() {
 
 
 
+    })
+
+    //afterChange : 슬라이드 변경 후 이벤트
+    slide.on('afterChange',function(event, slick, currentSlide){
+        console.log(currentSlide);
+        //currentSlide: 0 2 4 6 8 
+        // (0,1) (2,3) (4,5) (6,7) (8,9)
+        //   1     2     3    4      5
+        //---------------------------------------
+        //02468
+        //13579
+        //0.5 1.5 2.5 3.5 4.5
+        //12345
+        //현재 슬라이드 번호
+        let no =currentSlide +1
+        no = Math.round(no / 2)
+        $('#currentSlide').text(no)
     })
 
 
